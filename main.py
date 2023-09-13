900a0a67daeb4f9f359c96bd55d59ff1b8790bbf
lucascolas <lucas.colas13@gmail.com>
2023-09-13 18:51:08 -0400

    add some base code

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-import pygame
+import pygame 
 import numpy as np
+import sys
+pygame.init()
+
+def update_window():
+    pass
+
+
+def main():
+    Size_board = 4
+    Board_values = np.zeros((Size_board, Size_board))
+
+    # screen resolution
+    res = (720, 720)
+    screen = pygame.display.set_mode(res)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()  # Exit the program when the window is closed
+
+
+if __name__ == "__main__":
+    main()
