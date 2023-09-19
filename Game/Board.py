7d1686b8a0813d77fd5ab0d837486cc7f554b649
lucascolas <lucas.colas13@gmail.com>
2023-09-19 07:43:52 -0400

    add move down, move left, move right

--- Game/Board.py
+++ Game/Board.py
@@ -104,6 +104,109 @@ class Board:
                     self.board_values[row+1, col] = 0
                     merged_values[row] = True
                     merged_values[row+1] = True
+
+    def move_down(self):
+        """
+        
+        Move every value to the bottom of the board.
+        Stop moving when there's a value or the end of the board.
+        If there's a value, check if it's equal to the current value.
+        If it is, merge them.
+        
+        """
+
+        for col in range(self.board_size):
+            for row in range(self.board_size - 1, -1, -1):
+                # Shift down each value until we hit a value or the end of the board
+                if self.board_values[row, col] == 0:
+                    continue
+                current_val = self.board_values[row, col]
+                current_row = row
+                while current_row < self.board_size - 1 and self.board_values[current_row+1, col] == 0:
+                    current_row += 1
+                if current_row != row:
+                    self.board_values[current_row, col] = current_val
+                    self.board_values[row, col] = 0
+        
+        # Merge values if they're equal
+
+        for col in range(self.board_size):
+            merged_values = np.zeros(self.board_size, dtype=bool)
+            for row in range(self.board_size - 1, 0, -1):
+                if self.board_values[row, col] == self.board_values[row-1, col] and not merged_values[row] and not merged_values[row-1]:
+                    self.board_values[row, col] *= 2
+                    self.board_values[row-1, col] = 0
+                    merged_values[row] = True
+                    merged_values[row-1] = True
+
+    def move_left(self):
+        """
+        
+        Move every value to the left of the board.
+        Stop moving when there's a value or the end of the board.
+        If there's a value, check if it's equal to the current value.
+        If it is, merge them.
+        
+        """
+
+        for row in range(self.board_size):
+            for col in range(self.board_size):
+                # Shift left each value until we hit a value or the end of the board
+                if self.board_values[row, col] == 0:
+                    continue
+                current_val = self.board_values[row, col]
+                current_col = col
+                while current_col > 0 and self.board_values[row, current_col-1] == 0:
+                    current_col -= 1
+                if current_col != col:
+                    self.board_values[row, current_col] = current_val
+                    self.board_values[row, col] = 0
+        
+        # Merge values if they're equal
+
+        for row in range(self.board_size):
+            merged_values = np.zeros(self.board_size, dtype=bool)
+            for col in range(self.board_size - 1):
+                if self.board_values[row, col] == self.board_values[row, col+1] and not merged_values[col] and not merged_values[col+1]:
+                    self.board_values[row, col] *= 2
+                    self.board_values[row, col+1] = 0
+                    merged_values[col] = True
+                    merged_values[col+1] = True
+
+
+    def move_right(self):
+        """
+        
+        Move every value to the right of the board.
+        Stop moving when there's a value or the end of the board.
+        If there's a value, check if it's equal to the current value.
+        If it is, merge them.
+        
+        """
+
+        for row in range(self.board_size):
+            for col in range(self.board_size - 1, -1, -1):
+                # Shift right each value until we hit a value or the end of the board
+                if self.board_values[row, col] == 0:
+                    continue
+                current_val = self.board_values[row, col]
+                current_col = col
+                while current_col < self.board_size - 1 and self.board_values[row, current_col+1] == 0:
+                    current_col += 1
+                if current_col != col:
+                    self.board_values[row, current_col] = current_val
+                    self.board_values[row, col] = 0
+        
+        # Merge values if they're equal
+
+        for row in range(self.board_size):
+            merged_values = np.zeros(self.board_size, dtype=bool)
+            for col in range(self.board_size - 1, 0, -1):
+                if self.board_values[row, col] == self.board_values[row, col-1] and not merged_values[col] and not merged_values[col-1]:
+                    self.board_values[row, col] *= 2
+                    self.board_values[row, col-1] = 0
+                    merged_values[col] = True
+                    merged_values[col-1] = True
                 
                 
 
